385c255d355efe2f72447753c509a15fc2f104fc
CoolAlxs <alxs.jcc@gmail.com>
2024-01-18 19:38:06 -0500

    Se agrega el Proyecto

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import Swiper from 'swiper';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+  
+}
